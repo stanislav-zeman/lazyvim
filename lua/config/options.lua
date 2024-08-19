e2873501813143747772c935a2c9614cb58ff9d7
Stanislav Zeman <zeman@standa.dev>
2024-08-19 22:33:19 +0200

    feat(options): migrate options from lvim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,18 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.cmdheight = 1
+opt.wrap = true
+opt.ignorecase = true
+opt.cursorline = false
+
+opt.hlsearch = false
+opt.incsearch = false
+
+opt.swapfile = false
+opt.backup = false
