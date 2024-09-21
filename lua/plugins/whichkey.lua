3f0bde484347ceffa1c853b8288150191652fb7f
Stanislav Zeman <zeman@standa.dev>
2024-09-21 15:59:20 +0200

    feat(plugins): migrate whickkey bindings

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -4,18 +4,13 @@ return {
         event = "VeryLazy",
         keys = {
             {
-                "<leader>,", nil
+                "<leader>K", nil
             },
             {
-                "<leader>?",
-                function()
-                    require("which-key").show({ global = false })
-                end,
-                desc = "Buffer Local Keymaps (which-key)",
+                "<leader>l", nil
             },
             {
-                "<leader>f",
-                group = "Telescope"
+                "<leader>L", nil
             },
             {
                 "<leader>fn",
@@ -59,6 +54,60 @@ return {
                 "<leader>ps",
                 "<cmd>SwaggerPreview<CR>",
                 desc = "Swagger"
+            },
+            {
+                "<leader>pS",
+                "<cmd>SwaggerPreviewStop<CR>",
+                desc = "Swagger Stop"
+            },
+            {
+                "<leader>a",
+                group = "LSPSaga",
+            },
+            {
+                "<leader>aa",
+                "<cmd>Lspsaga code_action<CR>",
+                desc = "Action"
+            },
+            {
+                "<leader>ad",
+                "<cmd>Lspsaga goto_definition<CR>",
+                desc = "Goto Definition"
+            },
+            {
+                "<leader>aD",
+                "<cmd>Lspsaga hover_doc<CR>",
+                desc = "Docs"
+            },
+            {
+                "<leader>af",
+                "<cmd>Lspsaga finder<CR>",
+                desc = "Finder"
+            },
+            {
+                "<leader>ao",
+                "<cmd>Lspsaga outline<CR>",
+                desc = "Outline"
+            },
+            {
+                "<leader>ap",
+                "<cmd>Lspsaga peek_definition<CR>",
+                desc = "Peek"
+            },
+            {
+                "<leader>tj",
+                "<cmd>set filetype=json<CR>",
+                desc = "Set JSON"
+            },
+            {
+                "<leader>ty",
+                "<cmd>set filetype=yaml<CR>",
+                desc = "Set YAML"
+            },
+            {
+                "<leader>tt",
+                "<cmd>set filetype=toml<CR>",
+                desc = "Set TOML"
             },
         },
     }
