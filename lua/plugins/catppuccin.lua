2518775c16ddf8a280f5dfac278263f5e959b189
Stanislav Zeman <zeman@standa.dev>
2024-09-21 15:58:32 +0200

    feat(plugins): catppuccin add multiple integrations

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,19 +1,31 @@
 return {
     {
         "catppuccin/nvim",
+        lazy = true,
         name = "catppuccin",
         opts = {
             flavour = "frappe",
             transparent_background = true,
             integrations = {
                 beacon = true,
+                cmp = true,
                 flash = true,
                 gitsigns = true,
+                grug_far = true,
+                leap = true,
                 lsp_saga = true,
+                lsp_trouble = true,
+                markdown = true,
                 mason = true,
+                mini = true,
+                neotree = true,
                 noice = true,
                 notify = true,
                 rainbow_delimiters = true,
+                telescope = true,
+                treesitter = true,
+                treesitter_context = true,
+                semantic_tokens = true,
                 which_key = true,
             }
         }
