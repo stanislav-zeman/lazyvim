09b78ac026c94c44172ca4646342f228f1bd1a98
Stanislav Zeman <zeman@standa.dev>
2024-09-21 13:18:01 +0200

    feat(plugins): add flutter-tools

---
+++ lua/plugins/flutter-tools.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        'akinsho/flutter-tools.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'stevearc/dressing.nvim',
+        },
+        config = true,
+    }
+}
