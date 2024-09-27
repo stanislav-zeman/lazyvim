b8ef27a8c3a93e7ffb3c68a15470fc08d5892d9d
Stanislav Zeman <zeman@standa.dev>
2024-09-28 00:08:43 +0200

    feat(plugins): specify presets for noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -4,6 +4,9 @@ return {
     event = "VeryLazy",
     opts = {
       presets = {
+        bottom_search = false,
+        command_palette = false,
+        long_message_to_split = false,
         lsp_doc_border = true,
       },
     },
