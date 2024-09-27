3112e44ff06c5aed373fd3581fe9fa082b55ceef
Stanislav Zeman <zeman@standa.dev>
2024-09-28 00:31:19 +0200

    feat(plugins): update lspsaga options

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -3,13 +3,37 @@ return {
     "nvimdev/lspsaga.nvim",
     config = function()
       require("lspsaga").setup({
-        outline = {
-          close_after_jump = true,
+        callhierarchy = {
+          enable = false,
+        },
+        code_action = {
+          enable = false,
+        },
+        definitions = {
+          width = 0.8,
+          height = 0.8,
         },
         finder = {
+          max_height = 0.8,
+          right_width = 0.2,
+          left_width = 0.2,
+          default = "def+ref+imp",
           keys = {
             toggle_or_open = "<CR>",
           },
+        },
+        implement = {
+          enable = false,
+        },
+        outline = {
+          close_after_jump = true,
+        },
+        symbol_in_winbar = { -- breadcrumbs
+          enable = false,
+        },
+        ui = {
+          code_action = "",
+          devicons = true,
         },
       })
     end,
