cd1840c5a893e28e319908816f3f2fc9077c962e
Stanislav Zeman <zeman@standa.dev>
2024-10-20 12:59:08 +0200

    feat(plugins): update telescope options

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,15 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        layout_strategy = "vertical",
+        layout_config = {
+          height = 0.95,
+          width = 0.8,
+          preview_height = 0.65,
+        },
+      },
+    },
   },
 }
