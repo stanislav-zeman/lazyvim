bad3fe41ec24f8b88e9d063eea4f420b4355bcd1
Stanislav Zeman <zeman@standa.dev>
2024-10-29 13:24:35 +0100

    feat(plugins): change git-blame month specified

--- lua/plugins/git-blame.lua
+++ lua/plugins/git-blame.lua
@@ -4,7 +4,7 @@ return {
   opts = {
     enabled = false,
     message_template = " <summary> • <date> • <author> • <<sha>>",
-    date_format = "%d. %m. %Y",
+    date_format = "%d %B %Y",
     virtual_text_column = 1,
   },
 }
