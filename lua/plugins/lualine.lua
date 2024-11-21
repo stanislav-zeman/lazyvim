a901b9ce4fff59f06a4b4795c3e0171be87a4416
Stanislav Zeman <zeman@standa.dev>
2024-11-21 13:23:16 +0100

    feat(lualine): increase location padding

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -78,7 +78,7 @@ return {
         },
         lualine_z = {
           { "progress", separator = "", padding = { left = 1, right = 0 } },
-          { "location", padding = { left = 0, right = 1 } },
+          { "location", padding = { left = 1, right = 1 } },
         },
       },
     },
