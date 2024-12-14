d3b37dd3cec204263499549559cc9e730bfd0cb5
Stanislav Zeman <zeman@standa.dev>
2024-12-14 20:46:53 +0100

    feat(snacks): create config

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      ---@class snacks.animate.Config
+      animate = {
+        enabled = true,
+        duration = 10,
+        fps = 240,
+      },
+      bigfile = { enabled = true },
+      dashboard = { enabled = true },
+      indent = { enabled = true },
+      input = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      ---@class snacks.scroll.Config
+      scroll = {
+        enabled = true,
+        animate = {
+          duration = {
+            step = 5,
+            total = 250,
+          },
+          easing = "linear",
+        },
+        spamming = 10, -- threshold for spamming detection
+      },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+    },
+  },
+}
