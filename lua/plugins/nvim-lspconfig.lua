ce50f269c844b566b71f09fb76ad77c002b1a94a
Stanislav Zeman <zeman@standa.dev>
2025-01-21 14:56:12 +0100

    feat(lspconfig): setup golangci-lint

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,3 +1,18 @@
+local lspconfig = require("lspconfig")
+local configs = require("lspconfig/configs")
+
+if not configs.golangcilsp then
+  configs.golangcilsp = {
+    default_config = {
+      cmd = { "golangci-lint-langserver" },
+      root_dir = lspconfig.util.root_pattern(".git", "go.mod"),
+      init_options = {
+        command = { "golangci-lint", "run", "--out-format", "json", "--issues-exit-code=1" },
+      },
+    },
+  }
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
@@ -35,6 +50,9 @@ return {
               },
             },
           },
+        },
+        golangci_lint_ls = {
+          filetypes = { "go", "gomod" },
         },
       },
     },
