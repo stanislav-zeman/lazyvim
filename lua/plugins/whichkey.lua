2f6e7cdb3fcea718f283029b3c671d1ee7fa38d0
Stanislav Zeman <zeman@standa.dev>
2025-02-21 23:48:56 +0100

    feat(whichkey): set git blame inline toggle key shortcut

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -118,6 +118,11 @@ return {
         "<cmd>set filetype=toml<CR>",
         desc = "Set TOML",
       },
+      {
+        "<leader>ga",
+        "<cmd>GitBlameToggle<CR>",
+        desc = "Git Blame Inline",
+      },
     },
   },
 }
