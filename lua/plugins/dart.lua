6065c8625dee40c5c1c616105cd904746e4f3365
Stanislav Zeman <zeman@standa.dev>
2025-02-21 23:42:56 +0100

    feat(dart): configure dart plugins

---
+++ lua/plugins/dart.lua
@@ -0,0 +1,56 @@
+return {
+  recommended = function()
+    return LazyVim.extras.wants({
+      ft = "dart",
+      root = { "pubspec.yaml" },
+    })
+  end,
+  enabled = false,
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        dartls = {},
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "dart" } },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters = {
+        dart_format = {
+          args = { "format", "--line-length", "120" },
+        },
+      },
+      formatters_by_ft = {
+        dart = { "dart_format" },
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "sidlatau/neotest-dart",
+    },
+    opts = {
+      adapters = {
+        ["neotest-dart"] = {},
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = { ensure_installed = { "dart-debug-adapter" } },
+      },
+    },
+  },
+}
