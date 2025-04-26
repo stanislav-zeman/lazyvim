034475a0e5e6aaffefc71ef0b1d4f1686ac10ecf
Stanislav Zeman <zeman@standa.dev>
2025-04-26 03:24:55 +0200

    refactor: move disabled plugins to dedicated directory

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "plugins" },
+    { import = "plugins/disabled" },
   },
   ui = {
     border = "rounded",
