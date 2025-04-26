7ccd99c799cccafef83df7b44ebad313b304c332
Stanislav Zeman <zeman@standa.dev>
2025-04-26 02:49:18 +0200

    feat(smear-cursor): enable and configure plugin

---
+++ lua/plugins/smear-cursor.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      stiffness = 0.8,
+      trailing_stiffness = 0.6,
+      legacy_computing_symbols_support = true,
+    },
+  },
+}
