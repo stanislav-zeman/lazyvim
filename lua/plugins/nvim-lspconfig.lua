3c9a553c3e3621cea64758edcc8fef53ffa57fdf
Stanislav Zeman <zeman@standa.dev>
2025-08-04 01:25:30 +0200

    feat(lspconfig): update golangci lint json format command option

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -7,7 +7,7 @@ if not configs.golangcilsp then
       cmd = { "golangci-lint-langserver" },
       root_dir = lspconfig.util.root_pattern(".git", "go.mod"),
       init_options = {
-        command = { "golangci-lint", "run", "--out-format", "json", "--issues-exit-code=1" },
+        command = { "golangci-lint", "run", "--output.json.path", "stdout", "--issues-exit-code=1" },
       },
     },
   }
